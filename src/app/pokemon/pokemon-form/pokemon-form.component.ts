6ce0c5034bb496117abe0b108870a87d4c9087ab
Anna Cousin <anna.cousin@yahoo.fr>
2022-08-17 14:30:01 +0200

    requests

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -6,12 +6,13 @@ import { PokemonService } from '../pokemon.service';
 @Component({
   selector: 'app-pokemon-form',
   templateUrl: './pokemon-form.component.html',
-  styleUrls: ['./pokemon-form.component.css']
+  styleUrls: ['./pokemon-form.component.css'],
 })
 export class PokemonFormComponent implements OnInit {
   @Input()
   pokemon: Pokemon = new Pokemon;
   types: string[] = [];
+  isAddForm: boolean | undefined;
 
   constructor(
     private pokemonService: PokemonService,
@@ -20,6 +21,7 @@ export class PokemonFormComponent implements OnInit {
 
   ngOnInit() {
     this.types = this.pokemonService.getPokemonTypeList();
+    this.isAddForm = this.router.url.includes('add');
   }
 
   hasType(type: string): boolean {
@@ -48,7 +50,12 @@ export class PokemonFormComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log('submit');
-    this.router.navigate(['/pokemon', this.pokemon.id])
+    if(this.isAddForm) {
+      this.pokemonService.addPokemon(this.pokemon)
+      .subscribe((pokemon: Pokemon) => this.router.navigate(['/pokemon', this.pokemon.id]));
+    } else {
+      this.pokemonService.updatePokemon(this.pokemon)
+        .subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]));
+    }
   }
 }
